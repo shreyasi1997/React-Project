21e78996b031f629d437d240a91917015db52bd5
shreyasi1997 <shreyasichatterg@gmail.com>
2023-10-13 19:28:23 +0530

    message

--- src/loader/Loader.js
+++ src/loader/Loader.js
@@ -5,10 +5,9 @@ const Loader = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate a loading delay of 3 seconds (adjust as needed)
     const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
-    }, 90000); // Adjust the timeout to your desired duration in milliseconds
+    }, 90000); 
 
     return () => clearTimeout(loadingTimeout);
   }, []);
